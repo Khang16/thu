a83af9f4edd82fc6fc8ab3b77bf551e665a7b1e2
Khang <vuduongkhang16@gmail.com>
2023-03-25 16:02:46 +0700

    thay đổi

--- taoapp/lib/main.dart
+++ taoapp/lib/main.dart
@@ -7,16 +7,40 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: MaterialApp(
-          title: 'Hãy chọn giá đúng',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          debugShowCheckedModeBanner: false,
-          home: const HomePage()),
+    return MaterialApp(
+      //material và materialapp đều dngf để xây dựng giao diện người dùng theo phong cách googele
+      /* 
+      - MaterialApp cung cấp 1 cách tạo các đường dẫn thông qua Navigator
+      - Material được sử dụng để tạo button, text filed, checkbox,radio button, card
+    */
+      /*
+        Container: widget đơn giản nhất để chứa các thành phần UI khác, được sử dụng để tạo khung cho một phần của giao diện.
+
+        Text: widget để hiển thị văn bản trên màn hình.
+
+        Image: widget để hiển thị hình ảnh trên màn hình.
+
+        ListView: widget để tạo danh sách các phần tử có thể cuộn được.
+
+        RaisedButton và FlatButton: các widget nút để thực hiện hành động khi được nhấn.
+
+        TextField: widget để chấp nhận đầu vào từ người dùng.
+
+        Icon: widget để hiển thị biểu tượng trên màn hình.
+
+        Checkbox và Radio: các widget để chọn tùy chọn trong danh sách.
+
+        Card: widget để tạo các khung chứa chứa các thành phần UI khác.
+     */
+      title: 'Name Your Price',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
     );
   }
 }
--- taoapp/lib/page/home_page.dart
+++ taoapp/lib/page/home_page.dart
@@ -3,31 +3,101 @@ import 'package:flutter/services.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class ProductInfo {
+  final String name;
+  final int price;
+
+  ProductInfo(this.name, this.price);
+}
+
+final products = [
+  ProductInfo('Wireless mouse', 3),
+  ProductInfo('Keyboard', 5),
+  ProductInfo('Camera', 8),
+  ProductInfo('Speaker', 4),
+  ProductInfo('iPad', 1000),
+];
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentProductIndex = 0;
+  int? _inputtedPrice;
+  String _result = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // ignore: prefer_const_literals_to_create_immutables
+      // Scaffold dùng để xây dựng trang web
+      // Scaffold bao gồm :
+      // body (nội dung chính của màn hình)
+      //appBar (thanh đầu trang)
+      //bottomNavigationBar (thanh điều hướng dưới cùng)
+      //drawer (khung chứa menu bên trái)
       body: Center(
         child: Column(
           children: [
-            const Padding(padding: EdgeInsets.only(top: 300)),
-            const Text("BlueTooth Mouse"),
-            const Padding(padding: EdgeInsets.only(top: 30)),
+            const Padding(padding: EdgeInsets.only(top: 80)),
+            Text(products[_currentProductIndex].name),
+            const Padding(padding: EdgeInsets.only(top: 40)),
             SizedBox(
               width: 200,
               child: TextField(
+                key: const Key('priceInput'),
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                /* 
+                  inputFormatters là một thuộc tính của các widget như TextField hoặc TextFormField
+                  được sử dụng để giới hạn và định dạng đầu vào của người dùng.
+
+                  -FilteringTextInputFormatter.digitsOnly sử dụng để giới hạn đầu vapf của user chit chứa các số tuwd 0->9
+
+                */
+                onChanged: (value) {
+                  _inputtedPrice = int.tryParse(value);
+                },
               ),
             ),
-            const Padding(padding: EdgeInsets.only(top: 30)),
+            const Padding(padding: EdgeInsets.only(top: 40)),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  _result =
+                      _inputtedPrice == products[_currentProductIndex].price
+                          ? 'pass'
+                          : 'fail';
+                });
+              },
               child: const Text('Check'),
-            )
+            ),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: Text(
+                _result,
+                key: const Key('result'),
+                /*Khi bạn viết một test widget, bạn có thể sử dụng Key để tìm 
+                và truy cập một widget cụ thể trong tree widget. */
+              ),
+            ),
+            Visibility(
+              visible: _result.isNotEmpty,
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _result = '';
+                    if (_currentProductIndex < 4) {
+                      _currentProductIndex++;
+                    }
+                  });
+                },
+                child: const Text('Next'),
+              ),
+            ),
           ],
         ),
       ),
